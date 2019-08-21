43a9edde5cd67107e2f75b15967af2d767d3d564
me <me@me.home>
2019-08-21 15:29:31 +0300

    fix

--- lib/Oper/Rates.pm
+++ lib/Oper/Rates.pm
@@ -242,7 +242,6 @@ sub proto_add_edit_trigger{
 		my ($currency1,$currency2,$rate)=($self->query->param('r_currency1'),$self->query->param('r_currency2'),$self->query->param('r_rate'));
 		$row->{expr} = pow($rate, $RATE_FORMS{$currency1}->{$currency2});
 		$self->query->param('r_rate',$row->{expr});
-		die $row->{expr}, $currency1,$currency2,$rate;
 	}
    }
 
