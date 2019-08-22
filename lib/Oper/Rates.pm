6c5a3161f721939eb9802f8e4a07feced23645df
me <me@me.home>
2019-08-22 17:57:17 +0300

    fix rates

--- lib/Oper/Rates.pm
+++ lib/Oper/Rates.pm
@@ -249,9 +249,8 @@ sub proto_add_edit_trigger{
 	{
 		my ($currency1,$currency2,$rate)=($self->query->param('r_currency1'),$self->query->param('r_currency2'),$self->query->param('r_rate'));
 		
-		die $rate, $RATE_FORMS{$currency1}->{$currency2}, $rate**$RATE_FORMS{$currency1}->{$currency2};
-		$row->{expr} = lowpow($rate, $RATE_FORMS{$currency1}->{$currency2});
-		
+		$row->{expr} = $rate**$RATE_FORMS{$currency1}->{$currency2};
+                die $row->{expr};
 		$self->query->param('r_rate',$row->{expr});
 	}
    }
